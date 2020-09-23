239e4fd126a0eea211be080da8253a38c356b5df
Haydon Houghton <haydonhoughton@outlook.com>
2020-09-23 19:40:23 +0100

    Add navbar to home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Haydon Houghton",
+      headerLinks: [
+        {title: 'Home', path: '/'},
+        {title: 'Design', path: '/design'},
+        {title: 'About Me', path: '/about'}
+      ],
+
+      home: {
+        title: 'Title',
+        subtitle: 'subtitle',
+        projects: 'projects below'
+      },
+
+      design: {
+        title: 'Title',
+        description: 'Desc',
+        artwork: 'Art'
+      },
+
+      about: {
+        title: 'Title'
+      },
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Navbar bg="dark">
+            <Nav className="mr-auto">
+              {this.state.headerLinks.map(item => {
+                return (
+                  <Nav.Link href={item.path}>{item.title}</Nav.Link>
+                )
+              })}
+            </Nav>
+
+          </Navbar>
+        <Container fluid={true}>
+          {this.state.title}
+          
+        </Container>
+      </Router>
+    );
+  }
 }
 
 export default App;
