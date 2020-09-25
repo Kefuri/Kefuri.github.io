58f88dab42b388d38fa741dac5bbe38e5117ff37
Haydon Houghton <haydonhoughton@outlook.com>
2020-09-25 11:45:54 +0100

    Add card info details to home page

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+import CardInfo from './CardInfo'
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Image from 'react-bootstrap/Image';
 
 function Card(props) {
   return(
-    <Container>
-      <Row>
-        <Col>
-          {props.item.title}
-          <Image src={props.item.imgSrc} rounded />
-          {props.item.selected && <CardInfo title={props.item.title} subtitle={props.item.subtitle} />}
-        </Col>
-      </Row>
-    </Container>
-
-
+    <div className="d-inline-block c-card" onClick={(e) => props.click(props.item)}>
+      <img className="c-card-image" src={props.item.imgSrc} alt={props.item.imgSrc}></img>
+      {props.item.selected && <CardInfo title={props.item.title} subtitle={props.item.subtitle} repo={props.item.repo} deployed={props.item.deployed} />}
+    </div>
   )
 
 }
--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
+
+
 
 function CardInfo(props) {
-  
+
+  const style = useSpring({opacity:1 , from: {opacity: 0}});
+
+  return(
+    <animated.div className="c-card-info" style={style}>
+      <p className="c-card-title">{props.title}</p>
+      <p className="c-card-subtitle">{props.subtitle}</p>
+      <a href={props.deployed} target="_blank" rel="noopener noreferrer">View Live</a>
+      <br />
+      <a href={props.repo} target="_blank" rel="noopener noreferrer">See the Repo</a>
+      
+    </animated.div>
+  )
+
 }
+
+export default CardInfo
