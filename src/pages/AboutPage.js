a5ffbbd5df12bfc82fb81e9bc5bad7d64b865fc4
Haydon Houghton <haydonhoughton@outlook.com>
2020-10-14 13:57:11 +0100

    fix typos and poorly worded sentences

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,14 +10,14 @@ function AboutPage(props) {
     <Container>
       <Hero title={props.title} />
       <Content>
-        <Row>Hey! I'm Haydon, a budding software engineer. I've been writing code since 15, and conisder myself
+        <Row>Hey! I'm Haydon, a budding software engineer. I've been writing code since 15, and consider myself
         incredibly lucky to have found a passion that I could bring into my work-life this early on.</Row>
         <br />
         <Row>While most of my experience is in Python and Ruby, I learnt to pick-up new languages very easily through my time at Makers Academy.
-        The experience of learning new languages is already well-embedded in me though, after I learnt to speak Spanish in Spain while I lived there as a kid.</Row>
+        The experience of learning new languages is already well-embedded in me though, after I learnt to speak Spanish while living in Spain as a kid.</Row>
         <br />
        <Row> When I'm not writing code, I usually end up playing video games or watching anime. When those aren't entertaining me though,
-        I move to books or editing on photoshop</Row> 
+        I move to books or editing on Photoshop</Row>
       </Content>
     </Container>
     
